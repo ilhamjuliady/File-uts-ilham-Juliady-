591de02ba29432ee7fab538e71ecc2341ffc81f1
ilhamjuliady <49119247+ilhamjuliady@users.noreply.github.com>
2019-03-31 21:37:30 +0800

    Add files via upload

---
+++ uts1Ilham .cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Nama     : Ilham Juliady"<<endl;
+    cout<<"NIM      : 1829141005"<<endl;
+    cout<<"Kelas    : Teknik Komputer B"<<endl;
+    cout<<"ANgkatan : 2018"<<endl;
+    return 0;
+
+}
